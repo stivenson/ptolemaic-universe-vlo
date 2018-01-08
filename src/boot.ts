021626cc3e662cf96f1257c72999d64c8f22feea
Stivenson Rincón <stivenson.rpm@gmail.com>
2018-01-07 22:55:11 -0500

    refactor: animated entry for earth

--- src/boot.ts
+++ src/boot.ts
@@ -11,19 +11,34 @@ export default class Boot extends Phaser.State {
     create() {
 
         this.add.sprite(0, 0, 'space');
-        const earth = this.add.sprite(340, 240, 'earth');
+        const earth = this.add.sprite(340, -200, 'earth');
 
         const venereOrbit = this.add.graphics(310, 210);
-        venereOrbit.lineStyle(8, 0xD77302, 1);
+        venereOrbit.lineStyle(4, 0xD77302, 1);
         venereOrbit.drawCircle(100, 100, 500);
 
         const moonOrbit = this.add.graphics(310, 210);
-        moonOrbit.lineStyle(8, 0xD77302, 1);
+        moonOrbit.lineStyle(4, 0xD77302, 1);
         moonOrbit.drawCircle(100, 100, 250);
 
         this.game.state.start('Preloader', true, false);
         earth.inputEnabled = true;
-        earth.input.enableDrag(true);
+
+        this.add.tween(earth).to({y: 240}, 4000, Phaser.Easing.Bounce.Out, true);
+
+        this.loadEvents(earth);
+
+    }
+
+    loadEvents (earth) {
+
+        earth.events.onInputDown.add(this.clickEarth, this);
+
+    }
+
+    clickEarth () {
+
+        console.log('Click in earth');
 
     }
 
